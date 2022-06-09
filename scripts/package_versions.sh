c4521e22d53fd6c9852f9c97b5c300c73659a0f6
adilaman-github <69368605+adilaman-github@users.noreply.github.com>
2022-06-09 10:11:22 +0530

    Add files via upload

---
+++ scripts/package_versions.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+
+export CMAKE_LOCATION="http://www.cmake.org/files/v3.13/"
+export CMAKEVERSION_REQUIRED=cmake-3.12.4
+export CMAKEVERSION=cmake-3.13.4
+
+export GTEST_LOCATION="https://github.com/google/googletest/archive"
+export GTESTVERSION=release-1.8.1
+
+export GSL_LOCATION="http://ftpmirror.gnu.org/gsl/"
+export GSLVERSION=gsl-2.5
+
+export VC_LOCATION="https://github.com/VcDevel/Vc.git"
+export VC_TAG=1.4.1
+
+export ICU_LOCATION="http://download.icu-project.org/files/icu4c/63.1/"
+export ICUVERSION=icu4c-63_1
+
+export BOOST_LOCATION="https://boostorg.jfrog.io/artifactory/main/release/1.68.0/source/"
+export BOOSTVERSION=boost_1_68_0
+
+export PYTHIA6_LOCATION="https://root.cern.ch/download/"
+export PYTHIA6VERSION=pythia6
+
+export HEPMC_LOCATION="https://hepmc.web.cern.ch/hepmc/releases"
+export HEPMCVERSION=2.06.09
+
+export PYTHIA8_LOCATION="https://pythia.org/download/pythia82/"
+export PYTHIA8VERSION=pythia8240
+
+export XERCESC_LOCATION="https://archive.apache.org/dist/xerces/c/3/sources/"
+export XERCESCVERSION=3.1.2
+
+export MESA_LOCATION="ftp://ftp.freedesktop.org/pub/mesa/older-versions/7.x/7.10.3/"
+export MESAVERSION=MesaLib-7.10.3
+
+export GEANT4_LOCATION="https://github.com/Geant4/geant4.git/"
+export GEANT4VERSION=geant4-10.5-release
+export GEANT4VERSIONp=Geant4-10.5.1
+
+export ROOT_LOCATION="https://github.com/root-project/root"
+export ROOTVERSION=v6-16-00
+
+export GEANT3_LOCATION="https://github.com/FairRootGroup/geant3.git"
+export GEANT3BRANCH=v2-7_fairsoft
+
+export VGM_LOCATION="https://github.com/vmc-project/vgm"
+export VGMVERSION=v4-5
+export VGMDIR=VGM-4.5.0
+
+export GEANT4VMC_LOCATION="https://github.com/vmc-project/geant4_vmc.git"
+export GEANT4VMCBRANCH=v4-0-p1
+
+export MILLEPEDE_LOCATION="http://svnsrv.desy.de/public/MillepedeII/tags/"
+export MILLEPEDE_VERSION=V04-03-10
+
+export SODIUM_LOCATION="https://github.com/jedisct1/libsodium"
+export SODIUMBRANCH=1.0.17
+
+# export ZEROMQ_LOCATION="https://github.com/zeromq/libzmq"
+# If we bump higher than v4.2.5, we can use the upstream source again
+export ZEROMQ_LOCATION="https://github.com/FairRootGroup/libzmq"
+export ZEROMQ_VERSION=v4.3.1
+
+export PROTOBUF_LOCATION="https://github.com/google/protobuf/releases/download/v3.6.1"
+export PROTOBUF_VERSION=3.6.1
+
+export FLATBUFFERS_LOCATION="https://github.com/google/flatbuffers.git"
+export FLATBUFFERS_BRANCH=v1.11.0
+
+export MSGPACK_LOCATION="https://github.com/msgpack/msgpack-c"
+export MSGPACK_VERSION=cpp-3.1.1
+
+export NANOMSG_LOCATION="https://github.com/nanomsg/nanomsg"
+export NANOMSG_VERSION=1.1.5
+
+export YAMLCPP_LOCATION="https://github.com/jbeder/yaml-cpp"
+export YAMLCPP_VERSION=0.6.2
+
+############## Gent4 Data files
+
+export G4NDL_VERSION=G4NDL4.5
+export G4NDL_TAR=G4NDL.4.5.tar.gz
+
+export G4EMLOW_VERSION=G4EMLOW7.7
+export G4EMLOW_TAR=G4EMLOW.7.7.tar.gz
+
+export PhotonEvaporation_VERSION=PhotonEvaporation5.3
+export PhotonEvaporation_TAR=G4PhotonEvaporation.5.3.tar.gz
+
+export RadioactiveDecay_VERSION=RadioactiveDecay5.3
+export RadioactiveDecay_TAR=G4RadioactiveDecay5.3.tar.gz
+
+export G4SAIDDATA_VERSION=G4SAIDDATA2.0
+export G4SAIDDATA_TAR=G4SAIDDATA.2.0.tar.gz
+
+export G4PARTICLEXS_VERSION=G4PARTICLEXS1.1
+export G4PARTICLEXS_TAR=G4PARTICLEXS1.1.tar.gz
+
+export G4ABLA_VERSION=G4ABLA3.1
+export G4ABLA_TAR=G4ABLA3.1.tar.gz
+
+export G4INCL_VERSION=G4INCL1.0
+export G4INCL_TAR=G4INCL1.0.tar.gz
+
+export G4PII_VERSION=G4PII1.3
+export G4PII_TAR=G4PII.1.3.tar.gz
+
+export G4ENSDFSTATE_VERSION=G4ENSDFSTATE2.2
+export G4ENSDFSTATE_TAR=G4ENSDFSTATE2.2.tar.gz
+
+export RealSurface_VERSION=RealSurface2.1.1
+export RealSurface_TAR=G4RealSurface.2.1.1.tar.gz
+
+export G4TENDL_VERSION=G4TENDL1.3.2
+export G4TENDL_TAR=G4TENDL1.3.2.tar.gz
+
+############################
+
+
+export FAIRROOT_LOCATION="https://github.com/FairRootGroup/FairRoot.git"
+export FAIRROOTVERSION=dev
+
+export DDS_LOCATION="https://github.com/FairRootGroup/DDS.git"
+export DDSVERSION=2.2
+
+export ALIROOT_LOCATION="http://git.cern.ch/pub/AliRoot"
+export ALIROOTVERSION=master
+
+export FAIRLOGGER_LOCATION="https://github.com/FairRootGroup/FairLogger"
+export FAIRLOGGER_VERSION=v1.4.0
+
+export FAIRMQ_LOCATION="https://github.com/FairRootGroup/FairMQ"
+export FAIRMQ_VERSION=v1.4.3
+
+export OFI_LOCATION="https://github.com/ofiwg/libfabric"
+export OFI_TESTS_LOCATION="https://github.com/ofiwg/fabtests"
+export OFI_VERSION=v1.6.2
+
+export ASIOFI_LOCATION="https://github.com/FairRootGroup/asiofi"
+export ASIOFI_VERSION=v0.4.1
